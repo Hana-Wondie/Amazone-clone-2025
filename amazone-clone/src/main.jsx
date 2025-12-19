ca24c2ec788ec60ad89cbec848aa6fb363a4b611
hana <wondiehana@gmail.com>
2025-12-19 10:16:34 +0300

    modified

--- amazone-clone/src/main.jsx
+++ amazone-clone/src/main.jsx
@@ -1,17 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Dataprovider from './Componets/DataProvider/Dataprovider.jsx';
-import reducer from './Utility/reducer.js';
-import { initialState } from './Utility/reducer.js';
-import {HashRouter} from "react-router-dom"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Dataprovider from "./Componets/DataProvider/Dataprovider.jsx";
+import reducer from "./Utility/reducer.js";
+import { initialState } from "./Utility/reducer.js";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
-      <Dataprovider reducer={reducer} initialState={initialState}>
-        <App />
-      </Dataprovider>
-    </HashRouter>
+    <Dataprovider reducer={reducer} initialState={initialState}>
+      <App />
+    </Dataprovider>
   </StrictMode>
 );
--- amazone-clone/vite.config.js
+++ amazone-clone/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Amazone-clone-2025/amazone-clone/",
+  base:"/Amazone-clone-2025/",
   plugins: [react()],
 })
