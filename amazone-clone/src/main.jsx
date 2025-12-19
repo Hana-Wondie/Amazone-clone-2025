9fcacb60f851629df44d658b2d93f7e28fc4d221
hana <wondiehana@gmail.com>
2025-12-19 10:05:19 +0300

    modified

--- amazone-clone/src/main.jsx
+++ amazone-clone/src/main.jsx
@@ -5,10 +5,13 @@ import App from './App.jsx'
 import Dataprovider from './Componets/DataProvider/Dataprovider.jsx';
 import reducer from './Utility/reducer.js';
 import { initialState } from './Utility/reducer.js';
+import {HashRouter} from "react-router-dom"
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Dataprovider reducer={reducer} initialState={initialState}>
-      <App />
-    </Dataprovider>
+    <HashRouter>
+      <Dataprovider reducer={reducer} initialState={initialState}>
+        <App />
+      </Dataprovider>
+    </HashRouter>
   </StrictMode>
 );
