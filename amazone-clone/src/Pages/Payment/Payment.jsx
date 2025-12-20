05bde5be9dca6581ca4276b953e539f87a938ea7
hana <wondiehana@gmail.com>
2025-12-20 20:11:05 +0300

    payment UI

--- amazone-clone/src/Pages/Payment/Payment.jsx
+++ amazone-clone/src/Pages/Payment/Payment.jsx
@@ -1,12 +1,98 @@
-import React from 'react'
-import Layout from '../../Componets/Layout/Layout'
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 
+import React, { useContext, useState } from "react";
+import Layout from "../../Componets/Layout/Layout";
+import classes from "../Payment/Payment.module.css";
+import { DataContext } from "../../Componets/DataProvider/Dataprovider";
+import ProductCard from "../../Componets/Product/ProductCard";
+import CurrencyFormatter from "../../Componets/CurrencyFormatter.jsx/CurrencyFormatter";
 function Payment() {
+  const [{ user, basket }] = useContext(DataContext);
+  console.log(basket);
+  const totalItem = basket?.reduce((amount, item) => {
+    return item.amount + amount;
+  }, 0);
+  const total = basket.reduce((amount, item) => {
+    return item.price * item.amount + amount;
+  }, 0);
+  const [cardError, setCardError] =useState(null)
+  const stripe = useStripe();
+  const elements = useElements();
+  const handleChange = (event) => {
+event?.error.message? setCardError(event?.error.message) : setCardError(null)
+  }
   return (
     <Layout>
-      <div>Payment</div>
+      {/* header */}
+      <div className={classes.paymentHeader}>Checkout {totalItem} items</div>
+      {/* payment methos */}
+      <section className={classes.payment}>
+        {/* address */}
+        <div className={classes.flex}>
+          <h3>Delivery adress</h3>
+          <div>
+            <div>{user?.reloadUserInfo?.email}</div>
+            <div>123 React Lane</div>
+            <div>Chicago, IL</div>
+          </div>
+        </div>
+
+        <hr />
+
+        {/* product */}
+        <div className={classes.flex}>
+          <h3>Review items and delivery</h3>
+          <div>
+            {basket?.map((results) => (
+              <ProductCard
+                key={results.id}
+                id={results.id}
+                image={results.image}
+                title={results.title}
+                rate={results.rate}
+                count={results.count}
+                price={results.price}
+                flex={true}
+              />
+            ))}
+          </div>
+        </div>
+        <hr />
+        {/* card form */}
+        <div className={classes.flex}>
+          <h3>Payment methods</h3>
+          <div className={classes.paymentCardContainer}>
+            <div className={classes.paymentDetails}>
+              <form>
+                {/* error */}
+                {cardError && (
+                  <small
+                    style={{
+                      color: "red",
+                    }}
+                  >
+                    {cardError}
+                  </small>
+                )}
+                {/* card lement */}
+                <CardElement onChange={handleChange} />
+                {/* price */}
+                <div className={classes.paymentPrice}>
+                  <div>
+                    <span style={{display:"flex"}}>
+                      <p>Total order |</p>
+                      <CurrencyFormatter amount={total} />
+                    </span>
+                  </div>
+                  <button>Pay Now</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
 
-export default Payment
+export default Payment;
--- amazone-clone/src/Routing.jsx
+++ amazone-clone/src/Routing.jsx
@@ -6,15 +6,28 @@ import Orders from "./Pages/Orders/Orders"
 import Cart from "./Pages/Cart/Cart"
 import Results from "./Pages/Results/Results"
 import ProductDetail from "./Pages/ProductDetail/ProductDetail"
-
+import { CheckoutProvider } from "@stripe/react-stripe-js/checkout";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+const stripePromise = loadStripe(
+  "pk_test_51Sg9A6PbUnsHewp99crmt2hJHPFYwZVHlzcU7vR5neJoIGtb74raOejVCCSP4LcT57VO2HjL93RjAsPDGQJ6iDIx00ZAkmJCuO"
+);
 function Routing() {
+
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/payment" element={<Payment />} />
+          <Route path="/payment" element=
+          {
+          <Elements stripe={stripePromise}>
+            <Payment />
+
+          </Elements>
+          
+          } />
           <Route path="/orders" element={<Orders />} />
           <Route path="/categories/:catagoryName" element={<Results />} />
           <Route path="/products/:productId" element={<ProductDetail/>} />
